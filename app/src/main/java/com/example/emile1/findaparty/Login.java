85179b6f7cb4e54894380896eaaabb18e128e195
emileNetter <emile@netter.fr>
2016-11-17 16:47:08 +0100

    Ajout page register activity

--- app/src/main/java/com/example/emile1/findaparty/Login.java
+++ app/src/main/java/com/example/emile1/findaparty/Login.java
@@ -1,10 +1,13 @@
 package com.example.emile1.findaparty;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -16,7 +19,15 @@ public class Login extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button register = (Button) findViewById(R.id.btn_register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Register.class);
+                startActivity(i);
+            }
+        });
+        }
 
 
     }
-}
--- app/src/main/java/com/example/emile1/findaparty/Register.java
+++ app/src/main/java/com/example/emile1/findaparty/Register.java
@@ -1,13 +1,24 @@
 package com.example.emile1.findaparty;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
     }
+
+
 }
