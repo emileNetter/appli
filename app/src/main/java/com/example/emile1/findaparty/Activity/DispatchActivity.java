5b23026cb4c016a951e170e284b99aff5aad953f
emileNetter <emile@netter.fr>
2017-01-17 20:30:59 +0100

    Backend Done Parse Heroku MongoDb

---
+++ app/src/main/java/com/example/emile1/findaparty/Activity/DispatchActivity.java
@@ -0,0 +1,24 @@
+package com.example.emile1.findaparty.Activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.parse.ParseUser;
+
+public class DispatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Check if there is current user info
+        if (ParseUser.getCurrentUser() != null) {
+            // Start an intent for the logged in activity
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            // Start and intent for the logged out activity
+            startActivity(new Intent(this, Login.class));
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/emile1/findaparty/Activity/InitializeApplication.java
@@ -0,0 +1,22 @@
+package com.example.emile1.findaparty.Activity;
+
+import android.app.Application;
+import android.os.Bundle;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class InitializeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("110914")
+                .clientKey("&emilenetter33!")
+                .server("https://find-a-lan.herokuapp.com/parse") // The trailing slash is important.
+                .build());
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+    }
+}
