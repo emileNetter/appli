570d51f9688e6862c346e0b253e84ebac6ac995c
emileNetter <emile@netter.fr>
2017-02-04 16:41:11 +0100

    Action bar title

--- app/src/main/java/com/example/emile1/findaparty/Activity/MainActivity.java
+++ app/src/main/java/com/example/emile1/findaparty/Activity/MainActivity.java
@@ -40,7 +40,6 @@ public class MainActivity extends AppCompatActivity
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
