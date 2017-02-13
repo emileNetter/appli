26220ce48747a4ed0d573c77ca114519567f3367
emileNetter <emile@netter.fr>
2017-02-13 21:17:20 +0100

    Listview Test

--- app/src/main/java/com/example/emile1/findaparty/Activity/HomeFragment.java
+++ app/src/main/java/com/example/emile1/findaparty/Activity/HomeFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.emile1.findaparty.R;
 
@@ -14,7 +16,14 @@ import com.example.emile1.findaparty.R;
  */
 public class HomeFragment extends Fragment {
 
-
+    ListView mListView;
+    String[] prenoms = new String[]{
+            "Antoine", "Benoit", "Cyril", "David", "Eloise", "Florent",
+            "Gerard", "Hugo", "Ingrid", "Jonathan", "Kevin", "Logan",
+            "Mathieu", "Noemie", "Olivia", "Philippe", "Quentin", "Romain",
+            "Sophie", "Tristan", "Ulric", "Vincent", "Willy", "Xavier",
+            "Yann", "Zoé"
+    };
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -27,6 +36,12 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        mListView = (ListView) v.findViewById(R.id.listview_home);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, prenoms);
+        mListView.setAdapter(adapter);
+
+        return v;
     }
 }
